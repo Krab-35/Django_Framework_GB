eff44641c5dc1d05caaeaf255cb21b3b7124ea78
Krab-35 <k1r1ll@yandex.ru>
2021-08-14 20:48:30 +0300

    6*. Organized the loading of dynamic content to the products controller from a json file

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from json import load
 
 
 def index(request):
@@ -8,52 +9,11 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
+def load_json():
+    with open(r"./products/fixtures/products.json") as out_json:
+        return load(out_json)
+
+
 def products(request):
-    context = {
-        'title': 'GeekShop - Каталог',
-        'products': [
-            {
-                'name': 'Худи черного цвета с монограммами adidas Originals',
-                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
-                'image_address': 'vendor/img/products/Adidas-hoodie.png',
-                'price': 6090,
-                'added_to_basket': False,
-            },
-            {
-                'name': 'Синяя куртка The North Face',
-                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
-                'image_address': 'vendor/img/products/Blue-jacket-The-North-Face.png',
-                'price': 23725,
-                'added_to_basket': True,
-            },
-            {
-                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-                'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
-                'image_address': 'vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
-                'price': 3390,
-                'added_to_basket': False,
-            },
-            {
-                'name': 'Черный рюкзак Nike Heritage',
-                'description': 'Плотная ткань. Легкий материал.',
-                'image_address': 'vendor/img/products/Black-Nike-Heritage-backpack.png',
-                'price': 2340,
-                'added_to_basket': False,
-            },
-            {
-                'name': 'Худи черного цвета с монограммами adidas Originals',
-                'description': 'Гладкий кожаный верх. Натуральный материал.',
-                'image_address': 'vendor/img/products/Black-Dr-Martens-shoes.png',
-                'price': 13590,
-                'added_to_basket': False,
-            },
-            {
-                'name': 'Темно-синие широкие строгие брюки ASOS DESIGN',
-                'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.',
-                'image_address': 'vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png',
-                'price': 2890,
-                'added_to_basket': False,
-            },
-        ]
-    }
+    context = load_json()
     return render(request, 'products/products.html', context)
