8f91f73ef5e1a193c030588d3092170724664c9e
Krab-35 <k1r1ll@yandex.ru>
2021-09-02 21:41:27 +0300

    02. Implemented the CRUD admin panel mechanism for users

--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+
+from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
+
+from users.models import User
 
 
 def index(request):
     context = {'title': 'GeekShop - Admin'}
     return render(request, 'admins/index.html', context)
+
+
+def admin_users(request):
+    context = {'title': 'GeekShop - Пользователи', 'users': User.objects.all()}
+    return render(request, 'admins/admin-users.html', context)
+
+
+def admin_users_create(request):
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminRegistrationForm()
+    context = {'title': 'GeekShop - Создание пользователя', 'form': form}
+    return render(request, 'admins/admin-users-create.html', context)
+
+
+def admin_users_update(request, id):
+    selected_user = User.objects.get(id=id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(instance=selected_user, files=request.FILES, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=selected_user)
+    context = {'title': 'GeekShop - Редактирование пользователя', 'selected_user': selected_user, 'form': form}
+    return render(request, 'admins/admin-users-update-delete.html', context)
+
+
+def admin_user_delete(request, id):
+    user = User.objects.get(id=id)
+    user.delete()
+    return HttpResponseRedirect(reverse('admins:admin_users'))
