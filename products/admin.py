f39ae5fc1b963e84c1ac1144b37b59b3c993fe0b
Krab-35 <k1r1ll@yandex.ru>
2021-09-02 23:17:55 +0300

    05*. Converted the Django admin panel

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 
 from products.models import ProductCategory, Product
 
-admin.site.register(Product)
-admin.site.register(ProductCategory)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity')
+    fields = ('name', 'image', 'description', ('price', 'quantity'), 'category')
+    ordering = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    ordering = ('-name',)
+    search_fields = ('name',)
