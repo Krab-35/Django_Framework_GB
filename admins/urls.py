31179bc4af9cd1fd1b74c9e680097dad0664cd57
Krab-35 <k1r1ll@yandex.ru>
2021-09-07 23:59:15 +0300

    03-01. Transferred the CRUD admins/users controllers.

--- admins/urls.py
+++ admins/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
 
-from admins.views import index, admin_users, admin_users_create, admin_users_update, admin_users_delete,\
+from admins.views import UserIndexView, UserListView, UserCreateView, UserUpdateView, UserDeleteView,\
     admin_product_category, admin_product_category_create, admin_product_category_update, \
     admin_product_category_delete, admin_product, admin_product_create, admin_product_update, admin_product_delete
 
 app_name = 'admins'
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('users/', admin_users, name='admin_users'),
-    path('users-create/', admin_users_create, name='admin_users_create'),
-    path('users-update/<int:id>/', admin_users_update, name='admin_users_update'),
-    path('users-delete/<int:id>/', admin_users_delete, name='admin_users_delete'),
+    path('', UserIndexView.as_view(), name='index'),
+    path('users/', UserListView.as_view(), name='admin_users'),
+    path('users-create/', UserCreateView.as_view(), name='admin_users_create'),
+    path('users-update/<int:pk>/', UserUpdateView.as_view(), name='admin_users_update'),
+    path('users-delete/<int:pk>/', UserDeleteView.as_view(), name='admin_users_delete'),
 
     path('category/', admin_product_category, name='admin_product_category'),
     path('category-create/', admin_product_category_create, name='admin_product_category_create'),
--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
+
+from django.utils.decorators import method_decorator
+
+from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm, AdminProductCategory, AdminProduct
 
@@ -8,51 +14,65 @@ from users.models import User
 from products.models import Product, ProductCategory
 
 
-@user_passes_test(lambda u: u.is_staff)
-def index(request):
-    context = {'title': 'GeekShop - Admin'}
-    return render(request, 'admins/index.html', context)
+class UserIndexView(TemplateView):
+    template_name = 'admins/index.html'
+    extra_context = {'title': 'GeekShop - Admin'}
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
 
 
-@user_passes_test(lambda u: u.is_staff)
-def admin_users(request):
-    context = {'title': 'GeekShop - Пользователи', 'users': User.objects.all()}
-    return render(request, 'admins/admin-users.html', context)
+class UserListView(ListView):
+    model = User
+    template_name = 'admins/admin-users.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(UserListView, self).get_context_data(**kwargs)
+        context['title'] = 'GeekShop - Админ | Пользователи'
+        return context
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
 
 
-@user_passes_test(lambda u: u.is_staff)
-def admin_users_create(request):
-    if request.method == 'POST':
-        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admins:admin_users'))
-    else:
-        form = UserAdminRegistrationForm()
-    context = {'title': 'GeekShop - Создание пользователя', 'form': form}
-    return render(request, 'admins/admin-users-create.html', context)
+class UserCreateView(CreateView):
+    model = User
+    template_name = 'admins/admin-users-create.html'
+    form_class = UserAdminRegistrationForm
+    success_url = reverse_lazy('admins:admin_users')
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
 
 
-@user_passes_test(lambda u: u.is_staff)
-def admin_users_update(request, id):
-    selected_user = User.objects.get(id=id)
-    if request.method == 'POST':
-        form = UserAdminProfileForm(instance=selected_user, files=request.FILES, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admins:admin_users'))
-    else:
-        form = UserAdminProfileForm(instance=selected_user)
-    context = {'title': 'GeekShop - Редактирование пользователя', 'selected_user': selected_user, 'form': form}
-    return render(request, 'admins/admin-users-update-delete.html', context)
+class UserUpdateView(UpdateView):
+    model = User
+    template_name = 'admins/admin-users-update-delete.html'
+    form_class = UserAdminProfileForm
+    success_url = reverse_lazy('admins:admin_users')
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
 
 
-@user_passes_test(lambda u: u.is_staff)
-def admin_users_delete(request, id):
-    user = User.objects.get(id=id)
-    user.is_active = False
-    user.save()
-    return HttpResponseRedirect(reverse('admins:admin_users'))
+class UserDeleteView(DeleteView):
+    model = User
+    template_name = 'admins/admin-users-update-delete.html'
+    success_url = reverse_lazy('admins:admin_users')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.is_active = False
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
 
 
 @user_passes_test(lambda u: u.is_staff)
