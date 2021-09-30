9d3712aef3ab2a082416c2a94d77c0d70489249e
Krab-35 <k1r1ll@yandex.ru>
2021-09-30 19:24:51 +0300

    02. Implemented order statistics update via jQuery.

--- orders/views.py
+++ orders/views.py
@@ -39,6 +39,7 @@ class OrderCreate(CreateView):
                 for num, form in enumerate(formset.forms):
                     form.initial['product'] = basket_items[num].product
                     form.initial['quantity'] = basket_items[num].quantity
+                    form.initial['price'] = basket_items[num].product.price
                 basket_items.delete()
             else:
                 formset = OrderFormSet()
@@ -76,6 +77,10 @@ class OrderUpdate(UpdateView):
             formset = OrderFormSet(self.request.POST, instance=self.object)
         else:
             formset = OrderFormSet(instance=self.object)
+            for form in formset.forms:
+                if form.instance.pk:
+                    form.initial['price'] = form.instance.product.price
+            data['orderitems'] = formset
         data['orderitems'] = formset
         return data
 
