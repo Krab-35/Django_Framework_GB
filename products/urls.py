fef071180e0b8cfc5f45c888929d3efec517befc
Krab-35 <k1r1ll@yandex.ru>
2021-10-11 19:25:22 +0300

    03. Installed and configured the Memcached application

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from products.views import ProductListView
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='product'),
+    path('', cache_page(30)(ProductListView.as_view()), name='product'),
     path('<int:pk>/', ProductListView.as_view(), name='category'),
 ]
--- products/views.py
+++ products/views.py
@@ -3,6 +3,21 @@ from django.views.generic.list import ListView
 
 from products.models import Product, ProductCategory
 
+from django.conf import settings
+from django.core.cache import cache
+
+
+def get_links_menu():
+    if settings.LOW_CACHE:
+        key = 'links_menu'
+        links_menu = cache.get(key)
+        if links_menu is None:
+            links_menu = ProductCategory.objects.filter(is_active=True)
+            cache.set(key, links_menu)
+        return links_menu
+    else:
+        return ProductCategory.objects.filter(is_active=True)
+
 
 class IndexTemplateView(TemplateView):
     template_name = 'products/index.html'
@@ -15,7 +30,7 @@ class ProductListView(ListView):
     paginate_by = 3
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        self.categories = ProductCategory.objects.all()
+        self.categories = get_links_menu()
         context = super(ProductListView, self).get_context_data(**kwargs)
         context['title'] = 'GeekShop - Админ | Пользователи'
         context['categories'] = self.categories
